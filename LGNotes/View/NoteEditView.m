69dabe9f1029e65c552e521d794eebc3baea2e34
hend <309875551@qq.com>
2019-04-04 16:08:54 +0800

    处理版本问题

--- LGNotes/View/NoteEditView.m
+++ LGNotes/View/NoteEditView.m
@@ -13,7 +13,7 @@
 #import "LGNoteBaseTextView.h"
 #import "LGNoteConfigure.h"
 #import "NoteSourceDetailView.h"
-#import <YBImageBrowser/YBImageBrowser.h>
+#import "YBImageBrowser.h"
 #import "LGNoteImagePickerViewController.h"
 #import "LGNoteDrawBoardViewController.h"
 #import "LGNoteCutImageViewController.h"
